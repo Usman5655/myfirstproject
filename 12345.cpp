20b20cb79b81c30ed12a67605489e119e789b315
Usman5655 <muhammadusmanthathal@gmail.com>
2025-05-13 21:39:29 -0700

    Add files via upload

---
+++ 12345.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"enter three number:";
+	cin>>a>>b>>c;
+	if(a<b)
+	if(a<c)
+	cout <<a<<"is smallest number.";
+	else
+	cout<<c<<"is smallest number.";
+	else
+	if(b<c)
+	cout<<b<<"is smallest number.";
+	else
+	cout<<c<<"is smallest number.";
+}
